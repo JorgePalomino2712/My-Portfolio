6ec8681d32a12c482409a333d37ed656765727d1
Jorge2712 <jorgeps2712@gmail.com>
2025-10-14 00:43:06 -0500

    Añadiendo el modo dark

---
+++ main.js
@@ -0,0 +1,6 @@
+let botoncito = document.querySelector("#btn-dark");
+let todo = document.querySelector("#todo");
+
+botoncito.addEventListener("click", function () {
+todo.classList.toggle("dark");
+      });
