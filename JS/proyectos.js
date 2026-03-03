27cee19a8c23da1cae9b541505aa1ed8f6ee3127
Jorge2712 <jorgeps2712@gmail.com>
2026-03-03 12:06:27 -0500

    Actualizando

--- JS/proyectos.js
+++ JS/proyectos.js
@@ -1,28 +1,43 @@
-export let proyectos=[
+export let proyectos = [
     {
         titulo: "Blog Personal",
         descripcion: "Creacion de una Blog personal con un resumen de mi informacion, desarrollado solo con html",
-        imagen:"../imagenes/P1.png",
-        link:"https://jorgepalomino2712.github.io/PracticaCalificada1/",
+        imagen: "../imagenes/P1.png",
+        link: "https://jorgepalomino2712.github.io/PracticaCalificada1/",
     },
     {
         titulo: "Juego 3 en Raya",
         descripcion: "Juego de 3 en raya desarrollado usando html y css, solo parte del diseño, y siendo responsive para todos los dispositivos",
-        imagen:"../imagenes/TicToc.png",
-        link:"https://jorgepalomino2712.github.io/Tic-Tac-Toe-game/",
+        imagen: "../imagenes/TicToc.png",
+        link: "https://jorgepalomino2712.github.io/Tic-Tac-Toe-game/",
     },
     {
         titulo: "Formulario Epic Games",
         descripcion: "Formulario de Epic Games desarrollado solo con html y Css, siguiendo el diseño fiel de la pagina oficial",
-        imagen:"../imagenes/P2.png",
-        link:"https://jorgepalomino2712.github.io/PracticaCalificada2/",
+        imagen: "../imagenes/P2.png",
+        link: "https://jorgepalomino2712.github.io/PracticaCalificada2/",
     },
     {
         titulo: "Landing Page",
         descripcion: "Creando landing page de un gimnasio",
-        imagen:"../imagenes/P3.png",
-        link:"https://jorgepalomino2712.github.io/PracticaCalificada3/",
+        imagen: "../imagenes/P3.png",
+        link: "https://jorgepalomino2712.github.io/PracticaCalificada3/",
     },
-    
+    {
+        titulo: "Estancias en Finlandia",
+        descripcion: "Pagina para poder ver la disponibilidad de estancias en un Apartamento de Finlandia",
+        imagen: "../imagenes/P4.png",
+        link: "https://jorgepalomino2712.github.io/ProyectoFinal/"
+
+    },
+    {
+        titulo: "Practica 1",
+        descripcion: "Primera pagina desplegada en Vercel, utilizando React",
+        imagen: "../imagenes/P5.png",
+        link: "https://p1-n2.vercel.app/"
+
+    },
+
+
 
 ];
